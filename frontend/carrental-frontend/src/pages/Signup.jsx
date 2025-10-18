7a18c57ca286b6d3ae84ff0b3ce3b231d0bc8dcc
SkAhmadraja11 <2300032147@kluniversity.in>
2025-10-18 20:13:09 +0530

    success

--- frontend/carrental-frontend/src/pages/Signup.jsx
+++ frontend/carrental-frontend/src/pages/Signup.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8081/auth/signup", form);
+      const res = await axios.post("http://localhost:8082/auth/signup", form);
       alert(res.data);
       navigate("/");
     } catch {
